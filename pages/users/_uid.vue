7ae96b8bbbf701713b47e8ea557cc1e3f36cdab0
安達 健二 <kenji.183927@gmail.com>
2019-02-02 16:24:08 +0900

    axiosでQiitaの情報を取れた

---
+++ pages/users/_uid.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div
+    p
+      | /users/_id.vue
+      | User ID: {{ userId }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: this.$route.params.uid
+    }
+  }
+}
+</script>
