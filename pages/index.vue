7f4bba949b9d5175c9269ff950a91be36091cf67
安達 健二 <kenji.183927@gmail.com>
2019-02-02 19:48:16 +0900

    SECTION-015

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,32 @@
 <template lang="pug">
-  div
-    | pages/index
+  section.container
+    div
+      h3
+        | Nuxt.jsのタグがつけられた投稿の一覧
+      ul
+        li(v-for="item in items" :key= "item.id")
+          h4
+            span
+              | {{ item.title }}
+            small
+              span
+                | by
+              nuxt-link(:to="{ name : 'users-uid', params : { uid : item.user.id }}")
+                | {{ item.user.id }}
+          div
+            | {{ item.body.slice(0, 130) }}
+          p
+            a(:href = "item.url")
+              | {{ item.url }}
 </template>
 
 <script>
   export default {
-    async mounted() {
-      console.log( JSON.stringify(await this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, '') )
+    async asyncData( { app } ) {
+      const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+      return {
+        items
+      }
     }
   }
 </script>
--- pages/users/_uid.vue
+++ pages/users/_uid.vue
@@ -1,16 +1,35 @@
 <template lang="pug">
   div
+    h3
+      | {{ user.id }}
+    img(:src="user.profile_image_url")
     p
-      | /users/_id.vue
-      | User ID: {{ userId }}
+      | {{ user.description || "No description" }}
+    p
+      nuxt-link(to="/")
+        small
+          b
+            | TOPにもどる
+    h3
+      | {{ user.id }}さんの投稿一覧
+    ul
+      li(v-for="item in items" :key="item.id")
+        h4
+          span
+            | {{ item.title }}
+        div
+          | {{ item.body.slice(0, 130) }}
+        p
+          a(target="_blank" :href="item.url")
+            | {{ item.url }}
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      userId: this.$route.params.uid
-    }
+  async asyncData({ route, app }) {
+    const user = await app.$axios.$get(`https://qiita.com/api/v2/users/${route.params.uid}`)
+    const items = await app.$axios.$get(`https://qiita.com/api/v2/items?query=user:${route.params.uid}`)
+    return { user, items }
   }
 }
 </script>
